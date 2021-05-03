cb9ae17071339145a0eb7391a353d7867ea2f5e7
이승택 <seungdeng17@gmail.com>
2021-05-03 18:21:44 +0900

    feat: 프록시 서버 추가

---
+++ proxy-server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express();
+
+app.use("/api", createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true }));
+
+app.use(express.static("public"));
+app.use("/static", express.static(__dirname + "/public/build/static"));
+app.get("/*", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/build/index.html");
+});
+
+app.listen(3000);
