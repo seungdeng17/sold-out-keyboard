9c383020906e050fc9e97db9e02002b290b1c008
이승택 <seungdeng17@gmail.com>
2021-05-04 02:24:38 +0900

    chore: 소스 정리

--- server/routes/api/controller.js
+++ server/routes/api/controller.js
@@ -9,7 +9,7 @@ exports.addKeyboard = (req, res) => {
   const duplicateEmail = keyboardTable.filter((data) => data.email === email);
   const duplicateData = keyboardTable.filter((data) => data.email === email && data.number === number);
 
-  if (duplicateData.length) return res.status(200).json({ resultCode: "400", message: "이미 등록 된 정보에요." });
+  if (duplicateData.length) return res.status(200).json({ resultCode: "400", message: "이미 등록된 정보에요." });
   if (duplicateEmail.length >= 5) return res.status(200).json({ resultCode: "400", message: "메일 당 5건까지 등록돼요." });
   if (keyboardTable.length >= 100) return res.status(200).json({ resultCode: "400", message: "대기열이 모두 찼네요." });
 
@@ -20,7 +20,7 @@ exports.addKeyboard = (req, res) => {
     })
     .write();
 
-  return res.status(201).json({ resultCode: "100", message: "등록 되었어요." });
+  return res.status(201).json({ resultCode: "100", message: "등록되었어요." });
 };
 
 // [DELETE] 키보드, 이메일 알림 삭제
@@ -31,7 +31,7 @@ exports.removeKeyboard = (req, res) => {
   const keyboardTable = db.get("keyboard").value();
   const removeData = keyboardTable.filter((data) => data.email === email && data.number === number);
 
-  if (!removeData.length) return res.status(200).json({ resultCode: "400", message: "조회 된 정보가 없네요." });
+  if (!removeData.length) return res.status(200).json({ resultCode: "400", message: "조회된 정보가 없네요." });
 
   db.get("keyboard")
     .remove({
@@ -40,13 +40,13 @@ exports.removeKeyboard = (req, res) => {
     })
     .write();
 
-  return res.status(200).json({ resultCode: "100", message: "삭제 되었어요." });
+  return res.status(200).json({ resultCode: "100", message: "삭제되었어요." });
 };
 
 // [GET] 메일로 등록한 내용 조회
 exports.getKeyboard = (req, res) => {
   const { email } = req.query;
-  if (!email) return res.status(400).json({ resultCode: "400", message: "메일를 정확히 입력하세요." });
+  if (!email) return res.status(400).json({ resultCode: "400", message: "메일을 정확히 입력하세요." });
 
   const keyboardTable = db.get("keyboard").value();
   const duplicateEmail = keyboardTable.filter((data) => data.email === email);
