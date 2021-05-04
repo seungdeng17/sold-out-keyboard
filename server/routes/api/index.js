eeacdf285210d450579d8bb384ec8fa77f3b3a8f
이승택 <seungdeng17@gmail.com>
2021-05-04 13:16:49 +0900

    chore: 소스 정리

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,13 +1,8 @@
 const router = require("express").Router();
 const controller = require("./controller.js");
 
-// [POST] 키보드, 이메일 알람 추가
 router.post("/keyboard", controller.addKeyboard);
-
-// [DELETE] 키보드, 이메일 알람 삭제
 router.delete("/keyboard", controller.removeKeyboard);
-
-// [GET] 메일로 등록한 내용 조회
 router.get("/keyboard", controller.getKeyboard);
 
 module.exports = router;
